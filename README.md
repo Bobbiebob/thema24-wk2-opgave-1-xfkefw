6a3a275f44be9a6297e55349bbffb11e6f8db954
Bobbiebob <allardleinenga@gmail.com>
2020-05-20 15:02:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# thema24-wk2-opgave-1-xfkefw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/thema24-wk2-opgave-1-xfkefw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <p>Welkom, {{studentnaam}}.</p>
+    <p>Aantal uren gestudeerd: <span [textContent]="studietijd"></span></p>
+    <p>Aantal uren gelanterfanterd: <span [textContent]="lanterfantertijd"></span></p>
+    <p>Dagen tot aan de volgende vakantie: <b>{{ 365 - studiedagen }}</b></p>
+    <img [src]="employeeOfTheWeek.picture">
+`,
+styles: [` p { font-family:Calibri,Verdana,sans-serif; font-size:1.2rem; } `],
+})
+
+export class AppComponent  {
+  studentnaam = 'Henk de Boer';
+  studietijd = 18;
+  lanterfantertijd = (24 - this.studietijd);
+  studiedagen = Math.round((this.studietijd+this.lanterfantertijd)/24);
+  employeeOfTheWeek = {'picture': 'https://www.windowscentral.com/sites/wpcentral.com/files/styles/large/public/field/image/2017/10/cities-skylines-seattle-6.jpg?itok=PKg5QmSE',}
+}
